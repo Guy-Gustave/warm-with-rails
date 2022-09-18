da329816cf4fb1055ed7bf7a583e67864bc90653
Gustave <guymoustapha@gmail.com>
2022-09-18 07:08:44 +0200

    Render a form to edit password in the nav-link

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -5,6 +5,17 @@ class PasswordsController < ApplicationController
   end
 
   def update
+    if Current.user.update(password_params)
+      redirect_to root_path, notice: 'password updated'
     
+   else
+      rende :edit
+   end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
