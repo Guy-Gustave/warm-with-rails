8cb6c2a16e93092a1c2fffc1cfdd1d4e93a7104a
Gustave <guymoustapha@gmail.com>
2022-09-21 09:55:03 +0200

    Implement update reset password update flash notifications

--- app/controllers/password_reset_controller.rb
+++ app/controllers/password_reset_controller.rb
@@ -9,22 +9,32 @@ class PasswordResetController < ApplicationController
     if @user.present?
       #Send mail
       PasswordMailer.with(user: @user).reset.deliver_now
-
-      redirect_to root_path, notice: 'If this email does exist, check your inbox'
-      
-    else
-      redirect_to signup_path, notice: "toi bandit degages"
-      
     end
+
+    redirect_to root_path, notice: 'If the account of this email has found, check your inbox we have sent a link to reset your password'
+
 
   end
 
   def edit
-    
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to signin_path, alert: 'token has expired'
   end
 
   def update
-    
+    @user = User.find_signed(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to signin_path, notice: 'Your password was reset successifully, Please Sign_in'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
   
 end
