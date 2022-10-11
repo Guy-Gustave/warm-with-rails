40e542331e07f3363b970351ede94f6cf030d8c7
Gustave <guymoustapha@gmail.com>
2022-10-11 08:36:52 +0200

    Connect our rais app to twitter require logged in
    Disconnect to twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,17 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    render plain: "success"
+    twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+    twitter_account.update(
+      name: auth.info.name,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret,
+    )
+
+    redirect_to twitter_accounts_path, notice: 'Successifully connected to your accounts'
+  end
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
