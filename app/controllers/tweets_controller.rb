bd4625578f20f6f849adda1d21475f04c67d45bd
Gustave <guymoustapha@gmail.com>
2022-10-12 10:33:57 +0200

    Display and edit tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
-  
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
   def index
     @tweets = Current.user.tweets
   end
@@ -8,19 +9,39 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
-  
+
   def create
     @tweet = Current.user.tweets.new(tweet_params)
     if @tweet.save
-      redirect_to tweets_path, notice: 'Tweet screeted successsifully'
+      redirect_to tweets_path, notice: "Tweet was scheduled successfully"
     else
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Tweet was updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet was unscheduled"
   end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 end
