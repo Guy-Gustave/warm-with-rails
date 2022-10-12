bfa080a3b75215d16db43a1a542478bcf15e7198
Gustave <guymoustapha@gmail.com>
2022-10-12 09:17:49 +0200

    Add create method to tweet controller
    add form partial for the errors
    
    Create twitter form

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,4 +9,18 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
   
+  def create
+    @tweet = Current.user.tweets.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: 'Tweet screeted successsifully'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
 end
